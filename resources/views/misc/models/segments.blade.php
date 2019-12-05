24988fdac070fc325f7f91bd1be2151faa7e40ac
Felix Ayala <felix1262@gmail.com>
2019-12-05 15:34:37 -0300

    Fix segments view

--- resources/views/misc/models/segments.blade.php
+++ resources/views/misc/models/segments.blade.php
@@ -1,23 +1,19 @@
-{{-- <div class="row align-items-center">
-	<div class="col">
-		<!-- Nav -->
-		<ul class="nav nav-tabs nav-overflow header-tabs">
-			<li class="nav-item">
-				<a href="" class="nav-link active px-3">
-					Todos
-					<span class="badge badge-pill badge-soft-secondary ml-2">{{ $admins->count() }}</span>
-				</a>
-			</li>
-		</ul>
+@php
+	$model = $resource::newModel();
+@endphp
+@if (method_exists($model, 'segments') && count($model->segments()) > 0)
+	<div class="row align-items-center">
+		<div class="col">
+			<!-- Nav -->
+			<ul class="nav nav-tabs nav-overflow header-tabs">
+				@foreach ($resource::newModel()->segments() as $segment)
+					<li class="nav-item">
+						<a href="{{ resource('index') }}?segment={{ $segment->key() }}" class="nav-link {{ $segment->isActive() ? 'active' : '' }}">
+							{{ $segment->name }}
+						</a>
+					</li>
+				@endforeach
+			</ul>
+		</div>
 	</div>
-</div> --}}
-
-@isset($segments)
-	<select class="form-control form-segment-dropdown" onchange="var selected = this.options[this.selectedIndex].value; window.location = '{{ resource('index') }}?segment=' + selected;">
-		@foreach($segments as $key => $segment)
-			<option value="{{ $segment->key() }}" {{ $segment->isActive() ? 'selected' : '' }}>
-				{{ $segment->name }}
-			</option>
-		@endforeach
-	</select>
-@endisset
+@endif
