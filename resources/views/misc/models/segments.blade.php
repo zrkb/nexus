a87bd04d9bcbfc6037abc45b637c886826e62379
Felix Ayala <felix1262@gmail.com>
2019-12-05 15:54:07 -0300

    Check if $resource is present

--- resources/views/misc/models/segments.blade.php
+++ resources/views/misc/models/segments.blade.php
@@ -1,19 +1,21 @@
-@php
-	$model = $resource::newModel();
-@endphp
-@if (method_exists($model, 'segments') && count($model->segments()) > 0)
-	<div class="row align-items-center">
-		<div class="col">
-			<!-- Nav -->
-			<ul class="nav nav-tabs nav-overflow header-tabs">
-				@foreach ($resource::newModel()->segments() as $segment)
-					<li class="nav-item">
-						<a href="{{ resource('index') }}?segment={{ $segment->key() }}" class="nav-link {{ $segment->isActive() ? 'active' : '' }}">
-							{{ $segment->name }}
-						</a>
-					</li>
-				@endforeach
-			</ul>
+@isset ($resource)
+	@php
+		$model = $resource::newModel();
+	@endphp
+	@if (method_exists($model, 'segments') && count($model->segments()) > 0)
+		<div class="row align-items-center">
+			<div class="col">
+				<!-- Nav -->
+				<ul class="nav nav-tabs nav-overflow header-tabs">
+					@foreach ($model->segments() as $segment)
+						<li class="nav-item">
+							<a href="{{ resource('index') }}?segment={{ $segment->key() }}" class="nav-link {{ $segment->isActive() ? 'active' : '' }}">
+								{{ $segment->name }}
+							</a>
+						</li>
+					@endforeach
+				</ul>
+			</div>
 		</div>
-	</div>
-@endif
+	@endif
+@endisset
