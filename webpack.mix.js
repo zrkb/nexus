e3895598342cedf1b5bd32705842e05499c2c9c9
Felix Ayala <felix1262@gmail.com>
2020-01-14 15:47:07 -0300

    Move boxicons to application stylesheet

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,16 @@ const mix = require('laravel-mix');
 
 mix
 	.setPublicPath(path.resolve('./'))
+	// Fonts
+	.copy('node_modules/boxicons/fonts', 'resources/assets/fonts/vendor/boxicons')
+    .copy('resources/assets/fonts', 'public/assets/fonts')
+
+	// App
 	.js('resources/assets/js/app.js', 'public/assets/js')
 	.sass('resources/assets/scss/app.scss', 'public/assets/css')
+
+	// Copy
     .copy('resources/assets/css', 'public/assets/css')
-    .copy('resources/assets/fonts', 'public/assets/fonts')
     .copy('resources/assets/libs', 'public/assets/libs')
     .copy('resources/assets/img', 'public/assets/img')
     .version();
