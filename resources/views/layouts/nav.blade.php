5c59d5394536099760f724c492a94f322693c880
Felix Ayala <felix1262@gmail.com>
2020-03-19 18:22:47 -0300

    Refactor nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light d-none d-md-flex" id="topbar">
     <div class="container-fluid">
-        @include('nexus::layouts/menu-start')
-        @include('nexus::layouts/menu-end')
+        @include('nexus::layouts/nav-start')
+        @include('nexus::layouts/nav-end')
     </div>
 </nav>
