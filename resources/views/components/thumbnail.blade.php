fd727a0decfa8a2dabaf66f76748c20882c19387
Felix Ayala <felix1262@gmail.com>
2020-03-19 18:23:04 -0300

    Fix thumbnail

--- resources/views/components/thumbnail.blade.php
+++ resources/views/components/thumbnail.blade.php
@@ -1,3 +1,3 @@
-<div class="avatar {{ isset($size) ? (strlen($size) ? "-{$size}" : '') : 'avatar-sm' }} d-inline-block">
+<div class="avatar {{ isset($size) ? (strlen($size) ? "avatar-{$size}" : '') : 'avatar-sm' }} d-inline-block">
     <img src="{{ $media->getUrl() }}" alt="{{ $media->basename }}" class="avatar-img rounded-circle">
 </div>
