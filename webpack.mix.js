19c001730d04b79507cda9b3015d90b846261598
Felix Ayala <felix1262@gmail.com>
2020-04-22 02:43:51 -0400

    Update npm dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,18 +12,23 @@ const mix = require('laravel-mix');
  */
 
 mix
-	.setPublicPath(path.resolve('./'))
-	// Fonts
-	.copy('node_modules/boxicons/fonts', 'resources/assets/fonts/vendor/boxicons')
+    // Config
+    .setPublicPath(path.resolve('./'))
+
+    // Fonts
+    .copy('node_modules/boxicons/fonts', 'resources/assets/fonts/vendor/boxicons')
     .copy('resources/assets/fonts', 'public/assets/fonts')
 
-	// App
-	.js('resources/assets/js/app.js', 'public/assets/js')
-	.sass('resources/assets/scss/app.scss', 'public/assets/css')
-	.sass('resources/assets/scss/mango.scss', 'public/assets/css')
-
-	// Copy
+    // Copy
     .copy('resources/assets/css', 'public/assets/css')
     .copy('resources/assets/libs', 'public/assets/libs')
     .copy('resources/assets/img', 'public/assets/img')
-    .version();
+
+    // App
+    .js('resources/assets/js/app.js', 'public/assets/js')
+    .sass('resources/assets/scss/app.scss', 'public/assets/css')
+    .sass('resources/assets/scss/mango.scss', 'public/assets/css')
+
+    .setPublicPath('public') // Reset the public path before versioning
+    .version()
+;
