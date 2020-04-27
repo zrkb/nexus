4c5a8aac26e5c0d5b5c9770f81ac27d3ca87988c
Felix Ayala <felix1262@gmail.com>
2020-04-27 17:43:03 -0400

    Update page title markup

--- resources/views/misc/page-title.blade.php
+++ resources/views/misc/page-title.blade.php
@@ -1,31 +1,42 @@
 <!-- HEADER -->
 <div class="header">
-	<!-- Body -->
-	<div class="header-body">
-		<div class="row align-items-center">
-			<div class="col">
-				
-				@isset($preTitle)
-					<!-- Pretitle -->
-					<h6 class="header-pretitle">
-						{{ $preTitle }}
-					</h6>
-				@endisset
+    <!-- Body -->
+    <div class="header-body">
+        <div class="row align-items-center">
 
-				<!-- Title -->
-				<h1 class="header-title">
-					{{ $slot }}
-				</h1>
-			</div>
-			
-			<div class="col-auto">
-				{{ $superactions ?? '' }}
-			</div>
-		</div>
-		<!-- / .row -->
-		
-		{{ $segments ?? '' }}
-	</div>
-	<!-- / .header-body -->
+            @isset($start)
+                <div class="col-auto pr-0">
+                    {{ $start }}
+                </div>
+                <!-- END col -->
+            @endisset
+
+            <div class="col">
+                @isset($preTitle)
+                    <!-- Pretitle -->
+                    <h6 class="header-pretitle">
+                        {{ $preTitle }}
+                    </h6>
+                @endisset
+
+                <!-- Title -->
+                <h1 class="header-title">
+                    {{ $slot }}
+                </h1>
+            </div>
+            <!-- END col -->
+
+            @isset($superactions)
+                <div class="col-auto">
+                    {{ $superactions }}
+                </div>
+                <!-- END col -->
+            @endisset
+        </div>
+        <!-- END row -->
+
+        {{ $segments ?? '' }}
+    </div>
+    <!-- END header-body -->
 </div>
-<!-- / .header -->
+<!-- END header -->
