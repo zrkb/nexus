adabf41c46536ce28dc92fd0650de01cc6354b27
Felix Ayala <felix1262@gmail.com>
2020-06-06 12:51:56 -0400

    Add Form Group Switch component

---
+++ resources/views/components/form-group-switch.blade.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-12 col-md-6">
+
+        <div class="form-group">
+
+            @isset($title)
+                {{ form()->label($field, $title, ['class' => 'control-label']) }}
+            @endisset
+
+            @isset($description)
+                <small class="form-text text-muted">
+                    {{ $description }}
+                </small>
+            @endisset
+        </div>
+        <!-- END form-group -->
+
+    </div>
+    <!-- END col -->
+
+    <div class="col-12 col-md-6 text-right d-flex align-items-center justify-content-end">
+        <div class="form-group">
+            <!-- Switch -->
+            <div class="custom-control custom-switch d-inline-block">
+                {{ form()->checkbox($field, $value ?? 1, old() ? null : (isset($model) && $model ? $model->{$field} : null), ['class' => 'custom-control-input', 'id' => $field]) }}
+                <label class="custom-control-label" for="{{ $field }}"></label>
+            </div>
+        </div>
+        <!-- END form-group -->
+
+    </div>
+    <!-- END col -->
+</div>
+<!-- END row -->
