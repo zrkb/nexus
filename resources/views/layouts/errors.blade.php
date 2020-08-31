fb209c93d4ccd4a7f49568d689fce7cc78cd1497
Felix Ayala <felix1262@gmail.com>
2020-08-30 20:36:04 -0400

    Check if var exists

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,17 +1,17 @@
-@if ($errors->any())
-	<div class="alert alert-danger my-5">
-		<button type="button" class="close px-2" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
+@if (isset($errors) && $errors->any())
+    <div class="alert alert-danger my-5">
+        <button type="button" class="close px-2" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
 
-		@if ($errors->count() == 1)
-			{{ $errors->first() }}
-		@else
-			<ul class="m-0">
-				@foreach($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		@endif
-	</div>
+        @if ($errors->count() == 1)
+            {{ $errors->first() }}
+        @else
+            <ul class="m-0">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
 @endif
